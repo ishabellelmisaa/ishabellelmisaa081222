c74a0744565a2add764d9213f06d360348d9e7ad
ishabellelmisaa <118729310+ishabellelmisaa@users.noreply.github.com>
2023-01-08 23:26:34 +0700

    Create ArraySample2.java

---
+++ ArraySample2.java
@@ -0,0 +1,11 @@
+public class ArraySample2 {
+    public static void main( String[] args ){ 
+        int[] ages = new int[100]; 
+
+        for( int i=0; i<ages.length; i++ )
+        { 
+            System.out.print( ages[i] ); 
+            System.out.print( "\n" );
+        } 
+    } 
+}
