2c2cef2fdeee0a92e9db0a952b0078fb3e6d0cc1
ishabellelmisaa <118729310+ishabellelmisaa@users.noreply.github.com>
2023-01-08 23:23:45 +0700

    Create argument.java

---
+++ argument.java
@@ -0,0 +1,11 @@
+public class argument {
+    public static void main(String[] args)
+    {
+        System.out.println(""+args.length);
+        System.out.println(""+args[1]);
+        System.out.println(""+args[2]);
+        System.out.println(""+args[3]);
+        System.out.println(""+args[4]);
+        System.out.println(""+args[5]);
+    }
+}
